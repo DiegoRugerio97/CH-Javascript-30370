9a75b9deb05a946b132bef08ac4e9594005ddc5b
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-04-28 15:48:19 -0500

    Se agregan comentarios

--- Clase 6/Desafio Complementario/main.js
+++ Clase 6/Desafio Complementario/main.js
@@ -13,26 +13,32 @@ const DIAS_MES = 30.5;
 const DIAS_ANIO = 365;
 
 // Constantes para calculo de retencion de impuestos
-class limite{
-    constructor(limiteInferior,limiteSuperior, cuotaFija, porcentaje){
+// Clase limite
+class limite {
+    constructor(limiteInferior, limiteSuperior, cuotaFija, porcentaje) {
         this.limiteInferior = limiteInferior;
         this.limiteSuperior = limiteSuperior;
         this.cuotaFija = cuotaFija;
         this.porcentaje = porcentaje;
+        this.retencion;
+    }
+    calcularRetencion(salarioMensual) {
+        this.retencion = Math.round(((salarioMensual - this.limiteInferior) * this.porcentaje) + this.cuotaFija);
     }
 }
 
+// Array de objetos limite
 const LIMITES_ISR = [new limite(0, 644.58, 0, .0192),
-                    new limite(644.58, 5470.92, 12.38, 0.064),
-                    new limite(5470.92, 9614.66, 321.26, 0.1088),
-                    new limite(9614.66, 11176.62, 772.1, 0.16),
-                    new limite(11176.62, 13381.47, 1022.01, 0.1792),
-                    new limite(13381.47, 26988.5, 1417.12, 0.2136),
-                    new limite(26988.5, 42537.58, 4323.58, 0.2352),
-                    new limite(42537.58, 81211.25, 7980.73, 0.3),
-                    new limite(81211.25, 108281.67, 19582.83, 0.32),
-                    new limite(108281.67, 324845.01, 28245.36, 0.34),
-                    new limite(324845.01, Infinity, 101876.9, 0.35)];
+new limite(644.58, 5470.92, 12.38, 0.064),
+new limite(5470.92, 9614.66, 321.26, 0.1088),
+new limite(9614.66, 11176.62, 772.1, 0.16),
+new limite(11176.62, 13381.47, 1022.01, 0.1792),
+new limite(13381.47, 26988.5, 1417.12, 0.2136),
+new limite(26988.5, 42537.58, 4323.58, 0.2352),
+new limite(42537.58, 81211.25, 7980.73, 0.3),
+new limite(81211.25, 108281.67, 19582.83, 0.32),
+new limite(108281.67, 324845.01, 28245.36, 0.34),
+new limite(324845.01, Infinity, 101876.9, 0.35)];
 
 // *********************************************************************************************************************
 // Funciones de utilidad
@@ -82,12 +88,12 @@ const calcularAguinaldo = () => {
     // Si no, se calculan los dias proporcionales basado en los dias trabajados.
     let mayorCero = false;
     let salarioMensual;
-    while(!mayorCero){
+    while (!mayorCero) {
         salarioMensual = inputInt("¿De cuanto es tu salario mensual en pesos (MXN)?");
-        if(salarioMensual <= 0){
+        if (salarioMensual <= 0) {
             alert("Por favor introduce un valor mayor a 0.");
         }
-        else{
+        else {
             mayorCero = true;
         }
     }
@@ -115,17 +121,21 @@ const calcularAguinaldo = () => {
 // Funcion para el calculo de la retencion del Impuesto sobre la renta del salario.
 const calcularISR = () => {
     // Calculo de la retencion del ISR sobre el salario, basado en los limites establecidos por la ley federal del trabajo mexicana.
-    // Funcion con muchas constantes, Plan -> Convertir en array de objetos para optimizar constantes.
-    // Pasar logica de segmentado a una funcion aparte
-    // Por el momento seran hardcodeadas.
+    // Variable para la validacion
     let mayorCero = false;
+
+    // Variable para guardar el objeto limite despues de segmentar el salario
     let limiteAsignado;
+    // Variable para guardar el salario mensual del usuario
     let salarioMensual;
     while (!mayorCero) {
         salarioMensual = inputInt("¿De cuanto es tu salario mensual en pesos (MXN)?");
         if (salarioMensual >= 0) {
-            for(let limite of LIMITES_ISR){
-                if(salarioMensual > limite.limiteInferior && salarioMensual <= limite.limiteSuperior){
+            // Implementacion de logica de segmentado utilizando un array de objetos limite.
+            // Plan -> Optimizarlo utilizando .filter() o alguna otra funcion de arrays.
+            for (let limite of LIMITES_ISR) {
+                if (salarioMensual > limite.limiteInferior && salarioMensual <= limite.limiteSuperior) {
+                    // Regresa objeto con informacion del limite salarial.
                     limiteAsignado = limite;
                 }
             }
@@ -135,14 +145,14 @@ const calcularISR = () => {
             alert("Por favor introduce un valor mayor a 0.");
         }
     }
-    let retencion = Math.round(((salarioMensual - limiteAsignado.limiteInferior) * limiteAsignado.porcentaje) + limiteAsignado.cuotaFija);
+    // Metodo para el calculo de la retencion.
+    let retencion = limiteAsignado.calcularRetencion(salarioMensual);
     let mensaje = `
     Con un sueldo mensual de $${salarioMensual},
     tu retención de ISR según la ley federal de trabajo es de:
     $${retencion}, resultando en un ingreso de $${salarioMensual - retencion}.
     `;
     return mensaje;
-
 }
 
 // ****************************************************************************************************************+
