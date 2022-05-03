0c954ed414c64212c68fdd13a420521489393d11
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2022-05-03 16:51:03 -0500

    Creacion de funciones y funcionalidad del calculo de la declaracion anual

--- Proyecto Final/main.js
+++ Proyecto Final/main.js
@@ -5,7 +5,7 @@
 // Calculo de Aguinaldo - IMPLEMENTADO
 // Calculo de Retencion de impuestos - IMPLEMENTADO
 // Calculo de Vacaciones - CAMBIO A CALCULO DE DECLARACION ANUAL
-// Declaracion Anual
+// Declaracion Anual 
 
 // *********************************************************************************************************************
 
@@ -136,7 +136,7 @@ const calcularISR = () => {
         salarioMensual = inputInt("¿De cuanto es tu salario mensual en pesos (MXN)?");
         if (salarioMensual >= 0) {
             // Implementacion de logica de segmentado utilizando un array de objetos limite.
-            limiteAsignado = LIMITES_ISR.find((lim) => { return salarioMensual > lim.limiteInferior && salarioMensual <= lim.limiteSuperior});
+            limiteAsignado = LIMITES_ISR.find((lim) => { return salarioMensual > lim.limiteInferior && salarioMensual <= lim.limiteSuperior });
             // Metodo para el calculo de la retencion.
             limiteAsignado.calcularRetencion(salarioMensual);
             mayorCero = true;
@@ -155,9 +155,134 @@ const calcularISR = () => {
 
 // ****************************************************************************************************************+
 // Funcion para el calculo de la vacaciones.
-const calcularVacaciones = () => {
+// Clase para ingresos
+class Ingreso {
+    constructor(concepto, mes, monto) {
+        this.concepto = concepto;
+        this.mes = mes;
+        this.monto = monto;
+    }
+}
+
+// Clase para gasto deducible
+class GastoDeducible {
+    constructor(concepto, mes, monto) {
+        this.concepto = concepto;
+        this.mes = mes;
+        this.monto = monto;
+    }
+}
+
+// Funciones auxiliares para la declaracion anual
+// Registrar gastos deducibles
+const registrarGastoDeducible = (arrayGastos) => {
+    let gasto = new GastoDeducible(inputString("Concepto del gasto"),
+        inputString("Mes donde se realizo el gasto"),
+        inputFloat("Monto facturado del gasto deducible"));
+    arrayGastos.push(gasto);
+}
+
+// Registrar ingresos
+const registrarIngreso = (arrayIngreso) => {
+    let ingreso = new Ingreso(inputString("Concepto del ingreso"),
+        inputString("Mes donde se obtuvo el ingreso"),
+        inputFloat("Monto facturado del ingreso"));
+    arrayIngreso.push(ingreso);
+}
+
+// Reportar las listas
+const reportar = (array) => {
+    let mensaje = "";
+    if (array.length > 0) {
+        array.forEach(elem => {
+            mensaje += `Concepto: ${elem.concepto} - Mes: ${elem.mes} - Monto: $${elem.monto} \n`;
+        });
+    }
+    else {
+        mensaje = "Sin conceptos registrados."
+    }
+    return mensaje;
 
 }
+
+// Obtener la diferencia para determinar si es necesario realizar el pago de ISR
+const compararAcumulados = (arrayIngresos, arrayGastos) => {
+    const acumuladoIngresos = arrayIngresos.reduce((acumulado, ingreso) => acumulado + ingreso.monto, 0);
+    const acumuladoGastos = arrayGastos.reduce((acumulado, gasto) => acumulado + gasto.monto, 0);
+    const diferencia = Math.abs(acumuladoIngresos - acumuladoGastos);
+    return { acumuladoIngresos: acumuladoIngresos, acumuladoGastos: acumuladoGastos, diferencia: diferencia };
+}
+
+// Reporte de resultados
+const reportarResultados = (resultado) => {
+    let mensaje = `Con un acumulado de ingresos de $${resultado.acumuladoIngresos}
+Un acumulado de gastos deducibles de $${resultado.acumuladoGastos}
+Una diferencia de $${resultado.diferencia}`;
+    return mensaje;
+}
+
+// Funcion unificadora
+const construirDeclaracionAnual = (arrayIngresos, arrayGastos) => {
+    alert(reportar(arrayIngresos));
+    alert(reportar(arrayGastos));
+    if (arrayGastos.length > 0 && arrayIngresos.length > 0) {
+        let resultado = compararAcumulados(arrayIngresos, arrayGastos);
+        reportarResultados(resultado);
+    }
+    else {
+        alert("Favor de registrar ingresos y gastos");
+    }
+
+
+}
+
+
+const calcularDeclaracionAnual = () => {
+
+    let ingresos = [];
+    let gastosDeducibles = [];
+    let salir = false;
+    let submenu = `
+        Declaracion Anual
+        Elije una opción para empezar a calcular tu declaración anual.
+        Selecciona un tipo de concepto a registrar.
+        G - Gasto deducible
+        I - Ingreso
+        Introduce 'C' para continuar o 'S' para salir.
+        `;
+    do {
+        let opcionSeleccionada = inputString(submenu);
+        switch (opcionSeleccionada) {
+            case 'G':
+                registrarGastoDeducible(gastosDeducibles);
+                break;
+            case 'g':
+                registrarGastoDeducible(gastosDeducibles);
+                break;
+            case 'I':
+                registrarIngreso(ingresos);
+                break;
+            case 'i':
+                registrarIngreso(ingresos);
+                break;
+            case 'C':
+                construirDeclaracionAnual(ingresos, gastosDeducibles);
+                break;
+            case 'c':
+                construirDeclaracionAnual(ingresos, gastosDeducibles);
+            case 'S':
+                salir = true;
+                break;
+            case 's':
+                salir = true;
+                break;
+            default:
+                alert("Por favor, introduzca una opción válida.")
+                break;
+        }
+    } while (!salir)
+}
+
 
 let salirMenu = false;
 let menu = `
@@ -165,7 +290,7 @@ let menu = `
     Elije una opción para empezar el calculo del concepto.
     A - Aguinaldo.
     R - Retencion del ISR del salario.
-    V - Días de vacaciones.
+    D - Declaración anual.
     Introduce 'S' para salir.
 `;
 
@@ -185,11 +310,11 @@ do {
         case 'r':
             alert(calcularISR());
             break;
-        case 'V':
-            alert("Aun no implementado.");
+        case 'D':
+            calcularDeclaracionAnual();
             break;
-        case 'v':
-            alert("Aun no implementado.");
+        case 'd':
+            calcularDeclaracionAnual();
             break;
         case 'S':
             salirMenu = true;
